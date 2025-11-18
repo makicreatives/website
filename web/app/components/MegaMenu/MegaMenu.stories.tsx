411020c573f748f7f2a4cabdcfdad05e94333c9a
Mc Samuel Shoko <66551316+mcsamuelshoko@users.noreply.github.com>
2025-11-18 18:19:47 +0200

    feat: added `MegaMenu` component

---
+++ web/app/components/MegaMenu/MegaMenu.stories.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import type {Meta, StoryObj} from '@storybook/react';
+
+import {MegaMenu} from './MegaMenu';
+
+const meta: Meta<typeof MegaMenu> = {
+  component: MegaMenu,
+};
+
+export default meta;
+
+type Story = StoryObj<typeof MegaMenu>;
+
+export const Basic: Story = {args: {}};
---
+++ web/app/components/MegaMenu/tests/MegaMenu.test.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+import {MegaMenu} from '../MegaMenu';
+
+describe('<MegaMenu />', () => {});

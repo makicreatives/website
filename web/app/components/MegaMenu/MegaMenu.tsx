e8b7372d3369f3e9bb90c2e60fd8dc0d9ed83d46
Mc Samuel Shoko <66551316+mcsamuelshoko@users.noreply.github.com>
2025-11-19 18:02:06 +0200

    update MegaMenu.tsx, added logo & updated links styling

--- web/app/components/MegaMenu/MegaMenu.tsx
+++ web/app/components/MegaMenu/MegaMenu.tsx
@@ -1,108 +1,152 @@
+"use client"
+
 import React from 'react';
 
 // import styles from './MegaMenu.css';
+import Image from 'next/image'
+import primaryLogo from '@/app/icons/primaryLogo.svg'
+import Link from 'next/link';
+import { Button } from '@/app/components/';
 
 export interface MegaMenuProps {
-  prop?: string;
+  hTopColor?: string;
+  hBottomColor?: string;
+  activeLink: 'home' | 'portfolio' | 'pricing' | 'resources' | 'lets-work' | 'contact' | 'about' | '404'
 }
 
-export function MegaMenu({prop = 'default value'}: MegaMenuProps) {
-  return <nav className="bg-neutral-primary-soft border-default">
-      <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl p-4">
-          <a href="https://flowbite.com" className="flex items-center space-x-3 rtl:space-x-reverse">
-              <img src="https://flowbite.com/docs/images/logo.svg" className="h-7" alt="Flowbite Logo" />
-              <span className="self-center text-xl font-semibold whitespace-nowrap text-heading">Flowbite</span>
-          </a>
-          <button data-collapse-toggle="mega-menu-full" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-body rounded-lg md:hidden hover:bg-neutral-secondary-soft hover:text-heading focus:outline-none focus:ring-2 focus:ring-default" aria-controls="mega-menu-full" aria-expanded="false">
-              <span className="sr-only">Open main menu</span>
-              <svg className="w-6 h-6" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" stroke-linecap="round" stroke-width="2" d="M5 7h14M5 12h14M5 17h14"/></svg>
-          </button>
-          <div id="mega-menu-full" className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1">
-              <ul className="flex flex-col mt-4 font-medium md:flex-row md:mt-0 md:space-x-8 rtl:space-x-reverse">
-                  <li>
-                      <a href="#" className="block py-2 px-3 text-heading hover:text-fg-brand border-b border-light hover:bg-neutral-secondary-soft md:hover:bg-transparent md:border-0 md:hover:text-fg-brand md:p-0" aria-current="page">Home</a>
-                  </li>
-                  <li>
-                      <button id="mega-menu-full-dropdown-button" data-collapse-toggle="mega-menu-full-dropdown" className="flex items-center justify-between w-full py-2 px-3 font-medium text-heading border-b border-light md:w-auto hover:bg-neutral-secondary-soft md:hover:bg-transparent md:border-0 md:hover:text-fg-brand md:p-0">
-                          Company
-                          <svg className="w-4 h-4 ms-1.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 9-7 7-7-7"/></svg>
-                      </button>
-                  </li>
-                  <li>
-                      <a href="#" className="block py-2 px-3 text-heading hover:text-fg-brand border-b border-light hover:bg-neutral-secondary-soft md:hover:bg-transparent md:border-0 md:hover:text-fg-brand md:p-0">Marketplace</a>
-                  </li>
-                  <li>
-                      <a href="#" className="block py-2 px-3 text-heading hover:text-fg-brand border-b border-light hover:bg-neutral-secondary-soft md:hover:bg-transparent md:border-0 md:hover:text-fg-brand md:p-0">Resources</a>
-                  </li>
-                  <li>
-                      <a href="#" className="block py-2 px-3 text-heading hover:text-fg-brand border-b border-light hover:bg-neutral-secondary-soft md:hover:bg-transparent md:border-0 md:hover:text-fg-brand md:p-0">Contact</a>
-                  </li>
-              </ul>
-          </div>
+
+export function MegaMenu({ activeLink, hTopColor = 'bg-primary1B', hBottomColor = 'bg-primary' }: MegaMenuProps) {
+
+  const preLinkStyling: string = "block py-2 px-3 text-heading hover:text-primary1B  hover:bg-neutral-secondary-soft md:hover:bg-transparent  md:hover:text-primary1B md:p-0";
+
+  const activeLinkStyling: string = preLinkStyling + 'border-b-4 border-primary text-primary1B font-headlines text-headline-small font-bold';
+  const linkStyling: string = preLinkStyling + 'hover:text-primary1B hover:duration-150';
+
+
+  return <nav className="bg-primary0 text-white dark:text-off-white border-default">
+    <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl p-4">
+      <Link href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
+        <img src={"/images/site/logoicon.png"} className="h-12 inline-flex" alt="Maki Creative Studio Logo" />
+        <span className="self-center text-xl font-semibold whitespace-nowrap text-heading">Maki Creative Studio</span>
+      </Link>
+      <button data-collapse-toggle="mega-menu-full" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-body rounded-lg md:hidden hover:bg-neutral-secondary-soft hover:text-heading focus:outline-none focus:ring-2 focus:ring-default" aria-controls="mega-menu-full" aria-expanded="false">
+        <span className="sr-only">Open main menu</span>
+        <svg className="w-6 h-6" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" stroke-linecap="round" stroke-width="2" d="M5 7h14M5 12h14M5 17h14" /></svg>
+      </button>
+      <div id="mega-menu-full" className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1">
+        <ul className="flex flex-col mt-4 align-middle font-bold md:flex-row md:mt-0 md:space-x-8 rtl:space-x-reverse uppercase place-content-center">
+          {/* <li>
+            <Link href="#" className="block py-2 px-3 text-heading hover:text-primary1B border-b border-light hover:bg-neutral-secondary-soft md:hover:bg-transparent md:border-0 md:hover:text-primary1B md:p-0" aria-current="page">Home</Link>
+          </li> */}
+
+          <li className='place-content-center'><Link href={'/'} className={[linkStyling, (activeLink === 'home' ? activeLinkStyling : '')].join(' ')}>Home</Link></li>
+
+          {/* <li><Link href={'/lets-work/'} className={[linkStyling, (activeLink === 'lets-work' ? activeLinkStyling : '')].join(' ')}>Let&apos;s&nbsp;Work</Link></li> */}
+
+
+
+
+          <li className="place-content-center">
+            <button id="mega-menu-full-dropdown-button" data-collapse-toggle="mega-menu-full-dropdown" className="flex items-center justify-between w-full py-2 px-3 text-heading border-b border-light md:w-auto hover:bg-neutral-secondary-soft md:hover:bg-transparent md:border-0 md:hover:text-primary1B md:p-0 uppercase">
+              Services
+              <svg className="w-4 h-4 ms-1.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 9-7 7-7-7" /></svg>
+            </button>
+          </li>
+          <li className="place-content-center">
+            <Link href={'/portfolio'} className={[linkStyling, (activeLink === 'portfolio' ? activeLinkStyling : '')].join(' ')}>Portfolio</Link>
+          </li>
+          <li className="place-content-center">
+            <Link href={'/pricing'} className={[linkStyling, (activeLink === 'pricing' ? activeLinkStyling : '')].join(' ')}>Pricing</Link>
+          </li>
+          <li className="place-content-center">
+            <Link href={'/resources'} className={[linkStyling, (activeLink === 'resources' ? activeLinkStyling : '')].join(' ')}>Resources</Link>
+          </li>
+          {/* <li className="place-content-center">
+            <Link href={'/about'} className="block py-2 px-3 text-heading hover:text-primary1B border-b border-light hover:bg-neutral-secondary-soft md:hover:bg-transparent md:border-0 md:hover:text-primary1B md:p-0">About</Link>
+          </li> */}
+          <li className="">
+            {/* BOOK A DEMO */}
+            <Link href={'/lets-work'} >
+              <Button className="text-white border-textDark  bg-primary1B box-border border border-transparent hover:bg-primary hover:cursor-pointer  focus:ring-4 focus:ring-primary1B shadow-xs font-bold leading-5 rounded-full text-sm px-4 py-2.5 focus:outline-none uppercase">Book a call
+              </Button>
+            </Link>
+          </li>
+          <li className="">
+            {/* Sign in */}
+            <Link href={'/lets-work'} >
+              <Button className="text-white   bg-secondary hover:bg-secondary-950 hover:cursor-pointer focus:ring-4 focus:ring-secondary-700 shadow-xs font-bold leading-5 rounded-full text-sm px-4 py-2.5 focus:outline-none uppercase">Sign In
+              </Button>
+            </Link>
+          </li>
+        </ul>
       </div>
-      <div id="mega-menu-full-dropdown" className="mt-1 bg-neutral-primary-soft border-default shadow-xs border-y">
-          <div className="grid max-w-screen-xl px-4 py-5 mx-auto text-heading sm:grid-cols-2 md:grid-cols-3 md:px-6">
-              <ul aria-labelledby="mega-menu-full-dropdown-button">
-                  <li>
-                      <a href="#" className="block p-3 rounded-lg hover:bg-neutral-secondary-medium">
-                          <div className="font-semibold">Online Stores</div>
-                          <span className="text-sm text-body">Connect with third-party tools that you're already using.</span>
-                      </a>
-                  </li>
-                  <li>
-                      <a href="#" className="block p-3 rounded-lg hover:bg-neutral-secondary-medium">
-                          <div className="font-semibold">Segmentation</div>
-                          <span className="text-sm text-body">Connect with third-party tools that you're already using.</span>
-                      </a>
-                  </li>
-                  <li>
-                      <a href="#" className="block p-3 rounded-lg hover:bg-neutral-secondary-medium">
-                          <div className="font-semibold">Marketing CRM</div>
-                          <span className="text-sm text-body">Connect with third-party tools that you're already using.</span>
-                      </a>
-                  </li>
-              </ul>
-              <ul>
-                  <li>
-                      <a href="#" className="block p-3 rounded-lg hover:bg-neutral-secondary-medium">
-                          <div className="font-semibold">Online Stores</div>
-                          <span className="text-sm text-body">Connect with third-party tools that you're already using.</span>
-                      </a>
-                  </li>
-                  <li>
-                      <a href="#" className="block p-3 rounded-lg hover:bg-neutral-secondary-medium">
-                          <div className="font-semibold">Segmentation</div>
-                          <span className="text-sm text-body">Connect with third-party tools that you're already using.</span>
-                      </a>
-                  </li>
-                  <li>
-                      <a href="#" className="block p-3 rounded-lg hover:bg-neutral-secondary-medium">
-                          <div className="font-semibold">Marketing CRM</div>
-                          <span className="text-sm text-body">Connect with third-party tools that you're already using.</span>
-                      </a>
-                  </li>
-              </ul>
-              <ul className="hidden md:block">
-                  <li>
-                      <a href="#" className="block p-3 rounded-lg hover:bg-neutral-secondary-medium">
-                          <div className="font-semibold">Audience Management</div>
-                          <span className="text-sm text-body">Connect with third-party tools that you're already using.</span>
-                      </a>
-                  </li>
-                  <li>
-                      <a href="#" className="block p-3 rounded-lg hover:bg-neutral-secondary-medium">
-                          <div className="font-semibold">Creative Tools</div>
-                          <span className="text-sm text-body">Connect with third-party tools that you're already using.</span>
-                      </a>
-                  </li>
-                  <li>
-                      <a href="#" className="block p-3 rounded-lg hover:bg-neutral-secondary-medium">
-                          <div className="font-semibold">Marketing Automation</div>
-                          <span className="text-sm text-body">Connect with third-party tools that you're already using.</span>
-                      </a>
-                  </li>
-              </ul>
-          </div>
+
+    </div>
+    <section id="mega-menu-full-dropdown" className="mt-1 bg-neutral-primary-soft border-default shadow-xs border-y">
+      <div className="grid max-w-screen-xl px-4 py-5 mx-auto text-heading sm:grid-cols-2 md:grid-cols-3 md:px-6">
+        <ul aria-labelledby="mega-menu-full-dropdown-button">
+          <li>
+            <Link href="#" className="block p-3 rounded-lg hover:bg-neutral-secondary-medium">
+              <div className="font-semibold">Online Stores</div>
+              <span className="text-sm text-body">Connect with third-party tools that you're already using.</span>
+            </Link>
+          </li>
+          <li>
+            <Link href="#" className="block p-3 rounded-lg hover:bg-neutral-secondary-medium">
+              <div className="font-semibold">Segmentation</div>
+              <span className="text-sm text-body">Connect with third-party tools that you're already using.</span>
+            </Link>
+          </li>
+          <li>
+            <Link href="#" className="block p-3 rounded-lg hover:bg-neutral-secondary-medium">
+              <div className="font-semibold">Marketing CRM</div>
+              <span className="text-sm text-body">Connect with third-party tools that you're already using.</span>
+            </Link>
+          </li>
+        </ul>
+        <ul>
+          <li>
+            <Link href="#" className="block p-3 rounded-lg hover:bg-neutral-secondary-medium">
+              <div className="font-semibold">Online Stores</div>
+              <span className="text-sm text-body">Connect with third-party tools that you're already using.</span>
+            </Link>
+          </li>
+          <li>
+            <Link href="#" className="block p-3 rounded-lg hover:bg-neutral-secondary-medium">
+              <div className="font-semibold">Segmentation</div>
+              <span className="text-sm text-body">Connect with third-party tools that you're already using.</span>
+            </Link>
+          </li>
+          <li>
+            <Link href="#" className="block p-3 rounded-lg hover:bg-neutral-secondary-medium">
+              <div className="font-semibold">Marketing CRM</div>
+              <span className="text-sm text-body">Connect with third-party tools that you're already using.</span>
+            </Link>
+          </li>
+        </ul>
+        <ul className="hidden md:block">
+          <li>
+            <Link href="#" className="block p-3 rounded-lg hover:bg-neutral-secondary-medium">
+              <div className="font-semibold">Audience Management</div>
+              <span className="text-sm text-body">Connect with third-party tools that you're already using.</span>
+            </Link>
+          </li>
+          <li>
+            <Link href="#" className="block p-3 rounded-lg hover:bg-neutral-secondary-medium">
+              <div className="font-semibold">Creative Tools</div>
+              <span className="text-sm text-body">Connect with third-party tools that you're already using.</span>
+            </Link>
+          </li>
+          <li>
+            <Link href="#" className="block p-3 rounded-lg hover:bg-neutral-secondary-medium">
+              <div className="font-semibold">Marketing Automation</div>
+              <span className="text-sm text-body">Connect with third-party tools that you're already using.</span>
+            </Link>
+          </li>
+        </ul>
       </div>
+    </section>
+    <div id='hTop' className={['h-8 ', hTopColor].join(' ')}></div>
+    <div id='hBottom' className={['h-8 ', hBottomColor].join(' ')}></div>
   </nav>;
 }

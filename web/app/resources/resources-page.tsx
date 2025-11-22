af4a490e9400f2bc2b2b63d20e61235e031cde7b
Mc Samuel Shoko <66551316+mcsamuelshoko@users.noreply.github.com>
2025-11-22 03:19:36 +0200

    update resources-page.tsx

--- web/app/resources/resources-page.tsx
+++ web/app/resources/resources-page.tsx
@@ -4,7 +4,7 @@ import { MegaMenu, Faq, Footer } from "@/app/components";
 export default function ResourcesPage() {
 
     return <>
-        <MegaMenu activeLink='services'></MegaMenu>
+        <MegaMenu activeLink='resources'></MegaMenu>
         <section id="design" className="p-2 md:p-8"></section>
         <hr className="border-black dark:border-off-white1B" />
         <section id="production" className="p-2 md:p-8"></section>

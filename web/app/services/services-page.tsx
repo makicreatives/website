6a1833cc5affcf800b5c236f22742d0fe688d581
Mc Samuel Shoko <66551316+mcsamuelshoko@users.noreply.github.com>
2025-11-22 09:44:14 +0200

    update services-page.tsx, added boilerplate  layout

--- web/app/services/services-page.tsx
+++ web/app/services/services-page.tsx
@@ -1,5 +1,18 @@
-export default function ServicesPage(){
-    return <>
+import { Faq, Footer, MegaMenu } from "@/app/components";
+import { faqs } from "@/app/about/sections/faq";
 
+
+
+export default function ServicesPage() {
+    return <>
+        <MegaMenu activeLink='services'></MegaMenu>
+        <section id="design" className="p-2 md:p-8"></section>
+        <hr className="border-black dark:border-off-white1B" />
+        <section id="production"  className="p-2 md:p-8"></section>
+        <hr className="border-black dark:border-off-white1B" />
+        <section id="marketing" className="p-2 md:p-8"></section>
+        <hr className="border-black dark:border-off-white1B" />
+        <Faq faqs={faqs}></Faq>
+        <Footer></Footer>
     </>
 }

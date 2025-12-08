602fd5e691fcc0df2dcbad974a06dda77a9bfdff
Mc Samuel Shoko <66551316+mcsamuelshoko@users.noreply.github.com>
2025-12-09 00:56:24 +0200

    update web/app/sections/home/index.ts

--- web/app/sections/home/index.ts
+++ web/app/sections/home/index.ts
@@ -1,2 +1,5 @@
-export {default as HeroSection } from './hero';
-export {default as FeaturesSection} from './features';
+export { default as HeroSection } from './hero';
+export { default as FeaturesSection } from './features';
+export { default as ProcessSection } from './process';
+export { default as BenefitsSection } from './benefits';
+export { default as TestimonialsSection } from './testimonials';

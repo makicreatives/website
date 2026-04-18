e097966ce29be91ebf628f2af61a29cf68c10e8d
Mc Samuel Shoko <66551316+mcsamuelshoko@users.noreply.github.com>
2026-04-18 19:36:59 +0200

    update process.tsx, improved title
    - improved styling on images

--- web/app/sections/home/process.tsx
+++ web/app/sections/home/process.tsx
@@ -4,7 +4,7 @@ export default function ProcessSection() {
     return <section id="process" className={""}>
         <div className="border-y border-y-black dark:border-y-off-white  mb-4 md:mb-8 ">
             <div className="grid grid-cols-1 md:grid-cols-2 ">
-                <h2 className="h2 font-display  text-display-medium md:text-5xl p-8 border-r border-r-black dark:border-r-off-white">Using simple steps to deliver a stunning design</h2>
+                <h2 className="h2 font-display  text-display-medium md:text-5xl leading-tight p-8 border-r border-r-black dark:border-r-off-white">A clear process. <br /> A result you&apos;ll love</h2>
                 <div>
                     <div className="h-48 mx-auto overflow-hidden relative bg-gray-300 bg-opacity-40 ">
                         <div
@@ -20,7 +20,7 @@ export default function ProcessSection() {
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 border-t border-t-black dark:border-t-off-white">
                 <div className="border-r border-r-black dark:border-r-off-white">
                     <div className="p-8 h-8"></div>
-                    <div className={"relative h-96  border-y border-y-black dark:border-y-off-white duration-1000 ease-in-out hover:bg-radial-[at_25%_25%] from-sky-200 via-accent3/50 to-accent2/50 to-90%"}>
+                    <div className={"relative h-96  border-y border-y-black dark:border-y-off-white duration-1000 ease-in-out hover:bg-radial-[at_25%_25%] hover:from-[#D4AF37]/30 hover:via-[#C8A2FF]/40 hover:to-black/80"}>
                         <Image src={"/images/site/telephone.png"} alt={"telephone image"} layout="fill" className="bg-gray-3300 bg-opacity-40 mix-blend-luminosity object-cover"></Image>
                     </div>
                     <div className="p-8">
@@ -33,7 +33,7 @@ export default function ProcessSection() {
 
                 <div className="border-r border-r-black dark:border-r-off-white">
                     <div className="p-8 h-8"></div>
-                    <div className={"relative h-96  border-y border-y-black dark:border-y-off-white duration-1000 ease-in-out hover:bg-radial-[at_25%_25%] from-sky-200 via-accent3/50 to-accent2/50 to-90%"}>
+                    <div className={"relative h-96  border-y border-y-black dark:border-y-off-white duration-1000 ease-in-out hover:bg-radial-[at_25%_25%] hover:from-[#D4AF37]/30 hover:via-[#C8A2FF]/40 hover:to-black/80"}>
                         <Image src={"/images/site/arrow.png"} alt={"arrow image"} layout="fill" className="bg-gray-300 bg-opacity-40 mix-blend-luminosity object-cover"></Image>
                     </div>
                     <div className="p-8">
@@ -44,7 +44,7 @@ export default function ProcessSection() {
 
                 <div className="border-r border-r-black dark:border-r-off-white">
                     <div className="p-8 h-8"></div>
-                    <div className={"relative h-96  border-y border-y-black dark:border-y-off-white duration-1000 ease-in-out hover:bg-radial-[at_25%_25%] from-sky-200 via-accent3/50 to-accent2/50 to-90%"}>
+                    <div className={"relative h-96  border-y border-y-black dark:border-y-off-white duration-1000 ease-in-out hover:bg-radial-[at_25%_25%] hover:from-[#D4AF37]/30 hover:via-[#C8A2FF]/40 hover:to-black/80"}>
                         <Image src={"/images/site/cards.png"} alt={"cutout cards image"} layout="fill" className="bg-gray-300 bg-opacity-40  mix-blend-luminosity object-cover"></Image>
                     </div>
                     <div className="p-8">
@@ -55,8 +55,8 @@ export default function ProcessSection() {
 
                 <div className="border-r border-r-black dark:border-r-off-white">
                     <div className="p-8 h-8"></div>
-                    <div className={"relative h-96  border-y border-y-black dark:border-y-off-white duration-1000 ease-in-out hover:bg-radial-[at_25%_25%] from-sky-200 via-accent3/50 to-accent2/50 to-90%"}>
-                        <Image src={"/images/site/handshake.png"} alt={"handshake neon sign"} layout="fill" className="bg-gray-300 bg-opacity-40 mix-blend-luminosity object-cover"></Image>
+                    <div className={"relative h-96  border-y border-y-black dark:border-y-off-white duration-1000 ease-in-out hover:bg-radial-[at_25%_25%] hover:from-[#D4AF37]/30 hover:via-[#C8A2FF]/40 hover:to-black/80"}>
+                        <Image src={"/images/site/handshake.png"} alt={"handshake neon sign"} layout="fill" className="bg-gray-300 bg-opacity-0 mix-blend-luminosity object-cover"></Image>
                     </div>
                     <div className="p-8">
                         <p className="text-2xl font-technical uppercase font-bold mb-6">4. Launch & Support </p>

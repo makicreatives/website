7c258e0d291ef5d0869aff6c74fd97b873d0ffc6
Mc Samuel Shoko <66551316+mcsamuelshoko@users.noreply.github.com>
2026-04-20 21:52:00 +0200

    update design-page.tsx

--- web/app/services/design/design-page.tsx
+++ web/app/services/design/design-page.tsx
@@ -1,12 +1,19 @@
 import { Faq, Footer, MegaMenu } from "@/app/components";
 import { faqs } from "@/app/about/sections/faq";
+import { HeroSection } from "@/app/sections/services/design";
 
 
 
 export default function DesignPage() {
     return <>
         <MegaMenu activeLink='services'></MegaMenu>
-        
+        <HeroSection></HeroSection>
+        -- social proof
+        -- why
+        -- portfolio
+        -- features
+        -- testimonials
+        -- stats
         <hr className="border-black dark:border-off-white1B" />
         <Faq faqs={faqs}></Faq>
         <Footer></Footer>

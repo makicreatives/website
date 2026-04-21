932962bfbaf026189c360b5bc494fa1ec2c73bd1
Mc Samuel Shoko <66551316+mcsamuelshoko@users.noreply.github.com>
2026-04-21 12:01:54 +0200

    update production-page.tsx, added hero-section

--- web/app/services/production/production-page.tsx
+++ web/app/services/production/production-page.tsx
@@ -1,16 +1,13 @@
 import { Faq, Footer, MegaMenu } from "@/app/components";
 import { faqs } from "@/app/about/sections/faq";
+import { HeroSection } from "@/app/sections/services/production";
 
 
 
 export default function ProductionPage() {
     return <>
         <MegaMenu activeLink='services'></MegaMenu>
-        <section id="design" className="p-2 md:p-8"></section>
-        <hr className="border-black dark:border-off-white1B" />
-        <section id="production"  className="p-2 md:p-8"></section>
-        <hr className="border-black dark:border-off-white1B" />
-        <section id="marketing" className="p-2 md:p-8"></section>
+        <HeroSection></HeroSection>
         <hr className="border-black dark:border-off-white1B" />
         <Faq faqs={faqs}></Faq>
         <Footer></Footer>

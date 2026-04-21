5463f88c947d97186f312cb43af76b4640a8b9c2
Mc Samuel Shoko <66551316+mcsamuelshoko@users.noreply.github.com>
2026-04-21 12:52:09 +0200

    update design-page.tsx, marketing-page.tsx and production-page.tsx, added filler sections & templated sections

--- web/app/services/design/design-page.tsx
+++ web/app/services/design/design-page.tsx
@@ -1,6 +1,7 @@
 import { Faq, Footer, MegaMenu } from "@/app/components";
 import { faqs } from "@/app/about/sections/faq";
 import { HeroSection } from "@/app/sections/services/design";
+import { LogosSection, StatsSection, TestimonialsSection } from "@/app/sections/home";
 
 
 
@@ -8,12 +9,12 @@ export default function DesignPage() {
     return <>
         <MegaMenu activeLink='services'></MegaMenu>
         <HeroSection></HeroSection>
-        -- social proof
+        <LogosSection></LogosSection> {/* -- social proof */}
         -- why
         -- portfolio
         -- features
-        -- testimonials
-        -- stats
+        <TestimonialsSection></TestimonialsSection> {/* -- testimonials */}
+        <StatsSection></StatsSection> {/* -- stats */}
         <hr className="border-black dark:border-off-white1B" />
         <Faq faqs={faqs}></Faq>
         <Footer></Footer>
--- web/app/services/production/production-page.tsx
+++ web/app/services/production/production-page.tsx
@@ -1,6 +1,7 @@
 import { Faq, Footer, MegaMenu } from "@/app/components";
 import { faqs } from "@/app/about/sections/faq";
 import { HeroSection } from "@/app/sections/services/production";
+import { LogosSection, TestimonialsSection, StatsSection } from "@/app/sections/home";
 
 
 
@@ -8,6 +9,12 @@ export default function ProductionPage() {
     return <>
         <MegaMenu activeLink='services'></MegaMenu>
         <HeroSection></HeroSection>
+        <LogosSection></LogosSection> {/* -- social proof */}
+        -- why
+        -- portfolio
+        -- features
+        <TestimonialsSection></TestimonialsSection> {/* -- testimonials */}
+        <StatsSection></StatsSection> {/* -- stats */}
         <hr className="border-black dark:border-off-white1B" />
         <Faq faqs={faqs}></Faq>
         <Footer></Footer>

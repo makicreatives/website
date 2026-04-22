cc259c4baa598aa96f518f0f3f64a1644b75ea48
Mc Samuel Shoko <66551316+mcsamuelshoko@users.noreply.github.com>
2026-04-22 15:15:23 +0200

    updates indexes in services/[sections]
    - web/app/sections/services/design/index.ts,
    - web/app/sections/services/marketing/index.ts and
    - web/app/sections/services/production/index.ts

--- web/app/sections/services/marketing/index.ts
+++ web/app/sections/services/marketing/index.ts
@@ -1,1 +1,5 @@
 export { default as HeroSection } from './hero';
+export { default as FeaturesSection } from './features';
+export { default as PortfolioSection } from './portfolio';
+export { default as StatsSection } from './stats';
+export { default as WhyUsSection } from './why-us';

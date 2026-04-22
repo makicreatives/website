d6631989d1739596a98c3daed5763ce0739ca172
Mc Samuel Shoko <66551316+mcsamuelshoko@users.noreply.github.com>
2026-04-22 16:51:46 +0200

    update services-page.tsx, sorted page-section imports

--- web/app/services/services-page.tsx
+++ web/app/services/services-page.tsx
@@ -1,7 +1,7 @@
 import { Faq, Footer, MegaMenu } from "@/app/components";
 import { faqs } from "@/app/about/sections/faq";
-import { LogosSection, TestimonialsSection, StatsSection, PortfolioSection, CTASection } from "@/app/sections/home";
-import { FeaturesSection, HeroSection, WhyUsSection } from "@/app/sections/services";
+import { LogosSection, TestimonialsSection, CTASection } from "@/app/sections/home";
+import { FeaturesSection, HeroSection, WhyUsSection, PortfolioSection, StatsSection } from "@/app/sections/services";
 
 
 
@@ -10,11 +10,11 @@ export default function ServicesPage() {
         <MegaMenu activeLink='services'></MegaMenu>
         <HeroSection></HeroSection>
         <LogosSection></LogosSection> {/* -- social proof */}
-        <WhyUsSection></WhyUsSection> {/* -- why */}
-        <PortfolioSection></PortfolioSection>{/* -- portfolio */}
+        <WhyUsSection></WhyUsSection> 
+        <PortfolioSection></PortfolioSection> {/* -- portfolio */}
         <FeaturesSection></FeaturesSection>
         <TestimonialsSection></TestimonialsSection> {/* -- testimonials */}
-        <StatsSection></StatsSection> {/* -- stats */}
+        <StatsSection></StatsSection>
         <CTASection></CTASection>
         <Faq faqs={faqs}></Faq>
         <Footer></Footer>

de51204fc65767432884fd989043c0e895422013
Mc Samuel Shoko <66551316+mcsamuelshoko@users.noreply.github.com>
2026-04-22 14:03:30 +0200

    update sections/services/index.ts, added StatsSection import

--- web/app/sections/services/index.ts
+++ web/app/sections/services/index.ts
@@ -5,3 +5,4 @@ export * as Production from './production'
 export { default as HeroSection } from './hero';
 export { default as FeaturesSection } from './features';
 export { default as WhyUsSection } from './why-us';
+export { default as StatsSection } from './stats';

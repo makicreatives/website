dba71d0603d89d6e5c0c08ba69288bb6796d26fc
Mc Samuel Shoko <66551316+mcsamuelshoko@users.noreply.github.com>
2026-04-24 18:52:27 +0200

    fix sections/hero.tsx, updated catalogue link

--- web/app/sections/services/hero.tsx
+++ web/app/sections/services/hero.tsx
@@ -122,7 +122,7 @@ export default function HeroSection() {
                                         Connect more with your customers
                                     </p>
                                     <div className="mb-2 flex flex-wrap space-x-12 space-y-6">
-                                        <Link href={'/portfolio'}>
+                                        <Link href={'/services/production'}>
                                                         <Button className="text-white   bg-secondary hover:bg-secondary-950 hover:cursor-pointer focus:ring-4 focus:ring-secondary-700 shadow-xs font-bold leading-5 rounded-full text-sm px-8 py-2.5 focus:outline-none uppercase">View Catalogue
                                                         </Button>
                                                     </Link>

df25b122298f808f161171ff801ac3815f73db90
Mc Samuel Shoko <66551316+mcsamuelshoko@users.noreply.github.com>
2026-04-25 01:13:06 +0200

    update sections/pricing/index.ts, imported featureSection

--- web/app/sections/pricing/index.ts
+++ web/app/sections/pricing/index.ts
@@ -1,3 +1,4 @@
 export { default as HeroSection } from './hero';
 export { default as WhyUsSection } from './why-us';
 export { default as ProductOfferingSection } from './product-offering'
+export { default as FeaturesSection } from './features'
